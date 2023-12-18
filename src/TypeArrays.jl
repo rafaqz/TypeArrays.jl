8791220a17c759465768ad7ce85ca983ef014cae
Rafael Schouten <rafaelschouten@gmail.com>
2023-12-18 22:00:09 +0100

    add code

--- src/TypeArrays.jl
+++ src/TypeArrays.jl
@@ -1,5 +1,33 @@
 module TypeArrays
 
-# Write your package code here.
+using StaticArrays
+
+export TypeArray
+
+"""
+    TypeArray <: StaticArray
+
+An array that stores data entirely in the type.
+
+This can be useful for e.g. very fast `findfirst`
+when lookup values are also known at compile time.
+"""
+struct TypeArray{V,S,T,N} <: StaticArray{S,T,N} end
+TypeArray(A::AbstractArray{T,N}) where {T,N} = TypeArray{Tuple(A),Tuple{size(A)...},T,N}()
+TypeArray(V::Tuple) = TypeArray{V}()
+TypeArray{V}() where V = TypeArray{V,Tuple{length(V)}}()
+function TypeArray{V,S}() where {V,S}
+    T = mapreduce(typeof, promote_type, V)
+    N = length(Tuple(Size(S)))
+    TypeArray{V,S,T,N}()
+end
+
+Base.@propagate_inbounds function Base.getindex(::TypeArray{V}, i::Int) where V
+    V[i]
+end
+
+Base.findfirst(f::Function, ::TypeArray{V}; kw...) where V = findfirst(f, V; kw...)
+
+# TODO `searchsorted...`
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,20 @@ using TypeArrays
 using Test
 
 @testset "TypeArrays.jl" begin
-    # Write your tests here.
+    @test TypeArray((:a, :b)) == TypeArray{(:a, :b)}() == TypeArray([:a, :b])
+
+    m1 = TypeArray{(:a, :b, :c, :d),Tuple{2,2}}()
+    @test m1 isa AbstractMatrix
+    @test m1[4] === :d
+    @test m1[1, 1] === :a
+    @test findfirst(==(:c), m1) == 3
+    @test collect(m1) == [:a :c; :b :d]
+    @test collect(m1) == [:a :c; :b :d]
+
+    m2 = TypeArray([10 20; 30 40])
+    @test m2 isa AbstractMatrix
+    @test m2[2] == 30
+    @test m2[2, 2] == 40
+    @test findfirst(==(20), m2) == 3
+    @test collect(m2) == [10 20; 30 40]
 end
