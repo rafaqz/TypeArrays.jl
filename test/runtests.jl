37e168da72a918b2376fa954072e980be2d6b844
Rafael Schouten <rafaelschouten@gmail.com>
2023-12-19 14:20:23 +0100

    testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,17 +19,19 @@ using Test
     @test m2[2, 2] == 40
     @test collect(m2) == [10 20; 30 40]
 
-    for f in (findfirst, findlast, findall, findmin, findmax)
-        f = findlast
-        @test f(==(:b), v) == f(==(:b), vref)
-        @test f(==(:c), m1) == f(==(:c), collect(m1))
-        @test f(==(20), m2) == f(==(20), collect(m2))
+    @testset "find methods" begin
+        for f in (findfirst, findlast, findall, findmin, findmax)
+            f = findlast
+            @test f(==(:b), v) == f(==(:b), vref)
+            @test f(==(:c), m1) == f(==(:c), collect(m1))
+            @test f(==(20), m2) == f(==(20), collect(m2))
+        end
+        
+        @test findprev(==(:a), v , 2) == findprev(==(:a), collect(v) , 2)
+        @test findprev(==(:a), m1, CartesianIndex(2, 1)) == findprev(==(:a), collect(m1), CartesianIndex(2, 1))
+        @test findprev(==(20), m2, CartesianIndex(2, 2)) == findprev(==(20), collect(m2), CartesianIndex(2, 2))
+        @test findnext(==(:b), v , 1) == findnext(==(:b), collect(v) , 1)
+        @test findnext(==(:d), m1, CartesianIndex(2, 1)) == findnext(==(:d), collect(m1), CartesianIndex(2, 1))
+        @test findnext(==(40), m2, CartesianIndex(2, 1)) == findnext(==(40), collect(m2), CartesianIndex(2, 1))
     end
-    
-    @test findprev(==(:a), v , 2) == findprev(==(:a), collect(v) , 2)
-    @test findprev(==(:a), m1, CartesianIndex(2, 1)) == findprev(==(:a), collect(m1), CartesianIndex(2, 1))
-    @test findprev(==(20), m2, CartesianIndex(2, 2)) == findprev(==(20), collect(m2), CartesianIndex(2, 2))
-    @test findnext(==(:b), v , 1) == findnext(==(:b), collect(v) , 1)
-    @test findnext(==(:d), m1, CartesianIndex(2, 1)) == findnext(==(:d), collect(m1), CartesianIndex(2, 1))
-    @test findnext(==(40), m2, CartesianIndex(2, 1)) == findnext(==(40), collect(m2), CartesianIndex(2, 1))
 end
